0a91df0a61081e09c3161ea88f1127491f00781c
F1reMonster <f1remonster@proton.me>
2025-05-15 10:46:19 +0300

    inital commit

---
+++ src/js/app.js
@@ -0,0 +1,99 @@
+// import { useDynamicAdapt } from "./dynamicAdapt.js";
+// useDynamicAdapt();
+
+console.log("%cMade by FireMonster", "background:#8A2BE2;color:#fff;padding:4px 10px;font-weight:bold;border-radius:0.25rem;");
+
+document.addEventListener("DOMContentLoaded", function () {
+	const appHeight = () => {
+		const doc = document.documentElement;
+		doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+	};
+
+	const menuBurgerBtn = document.querySelector(".header__menu");
+	const menuBurger = document.querySelector(".menu__burger");
+	const menuBurgerClose = document.querySelector(".menu__burger .menu-close");
+	
+
+	window.addEventListener("resize", appHeight);	
+
+	
+
+	// menu open/close
+	menuBurgerBtn && menuBurgerBtn.addEventListener("click", () => {
+		menuBurger.classList.add("active");
+	});
+
+	menuBurgerClose && menuBurgerClose.addEventListener("click", () => {
+		menuBurger.classList.remove("active");
+	});
+
+	menuBurgerBtn && document.addEventListener("click", function (e) {
+		if (!menuBurgerBtn.contains(e.target) && !menuBurger.contains(e.target)) {
+			menuBurger.classList.remove("active");
+		}
+	});
+
+	
+	
+
+	function initLanguageSwitcher() {
+		const switcher = document.querySelector(".language__swither");
+		const menu = document.querySelector(".language");
+		const currentValue = document.querySelector(".language__current-value");
+		const languageItems = document.querySelectorAll(".language__submenu-item a");
+
+		switcher?.addEventListener("click", (e) => {
+			e.stopPropagation();
+			const expanded = switcher.getAttribute("aria-expanded") === "true";
+			switcher.setAttribute("aria-expanded", !expanded);
+			menu.classList.toggle("active");
+
+			languageItems.forEach((item) => {
+				item.setAttribute("tabindex", expanded ? "-1" : "0");
+			});
+		});
+
+		languageItems.forEach((item) => {
+			item.addEventListener("click", (e) => {
+				e.preventDefault();
+				const lang = item.getAttribute("lang");
+				const flagSrc = item.querySelector("img").src;
+				const languageName = item.getAttribute("data-language-name");
+				const baseAriaLabel = item.getAttribute("data-base-aria-label"); // Беремо з поточного лінка
+
+				// Оновлюємо поточне значення
+				currentValue.innerHTML = `<img src="${flagSrc}" aria-hidden="true" />`;
+				currentValue.setAttribute("lang", lang);
+				currentValue.setAttribute("xml:lang", lang);
+
+				// Оновлюємо aria-label світчера
+				switcher.setAttribute("aria-label", `${baseAriaLabel} ${languageName}`);
+
+				// Закриваємо меню
+				menu.classList.remove("active");
+				switcher.setAttribute("aria-expanded", "false");
+
+				// Оновлюємо стан елементів меню
+				languageItems.forEach((i) => {
+					i.setAttribute("aria-selected", i === item ? "true" : "false");
+					i.setAttribute("tabindex", "-1");
+				});
+			});
+		});
+
+		document.addEventListener("click", (e) => {
+			if (!menu.contains(e.target)) {
+				menu.classList.remove("active");
+				switcher?.setAttribute("aria-expanded", "false");
+				languageItems.forEach((item) => item.setAttribute("tabindex", "-1"));
+			}
+		});
+	}
+
+	initLanguageSwitcher();
+
+	appHeight();
+	
+
+	
+});
---
+++ vite.config.js
@@ -0,0 +1,146 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import path from "path";
+import fs from "fs";
+import * as sass from "sass";
+// import tailwindcss from "@tailwindcss/vite";
+import handlebars from "vite-plugin-handlebars";
+// import gday from "./src/data/gday";
+// import girls from "./src/data/girls";
+
+function getHtmlEntryFiles(srcDir) {
+	const entry = {};
+
+	function traverseDir(currentDir) {
+		const files = fs.readdirSync(currentDir);
+
+		files.forEach((file) => {
+			const filePath = path.join(currentDir, file);
+			const isDirectory = fs.statSync(filePath).isDirectory();
+
+			if (isDirectory) {
+				// If it's a directory, recursively traverse it
+				traverseDir(filePath);
+			} else if (path.extname(file) === ".html") {
+				// If it's an HTML file, add it to the entry object
+				const name = path.relative(srcDir, filePath).replace(/\..*$/, "");
+				entry[name] = filePath;
+			}
+		});
+	}
+
+	traverseDir(srcDir);
+
+	return entry;
+}
+
+function transformImagePaths(isProduction) {
+	return {
+		name: "transform-image-paths",
+		transformIndexHtml(html) {
+			if (!isProduction) return html;
+
+			return html.replace(/(href)="(?:\.\/)?(img\/[^"]+\.(?:webp|png|jpg|jpeg|gif|svg|ico))"/g, '$1="./assets/$2"');
+		},
+	};
+}
+
+// при збірці прибирає crossorigin
+// const noAttr = () => {
+// 	return {
+// 		name: "no-attribute",
+// 		transformIndexHtml(html) {
+// 			return html.replace(`crossorigin`, "");
+// 		},
+// 	};
+// };
+
+export default defineConfig({
+	root: "src",
+	base: "./",
+	build: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					implementation: sass,
+					api: "modern-compiler",
+				},
+			},
+		},
+		rollupOptions: {
+			input: getHtmlEntryFiles("src"),
+			output: {
+				// assetFileNames: (assetInfo) => {
+				// 	if (/\.css$/.test(assetInfo.name)) {
+				// 		return `assets/css/styles.css`;
+				// 	}
+
+				// 	if (/\.png$|\.jpe?g$|\.gif$|\.webp$|\.ico$/.test(assetInfo.name)) {
+				// 		return "assets/img/[name][extname]";
+				// 	}
+				// 	if (/\.svg$/.test(assetInfo.name)) {
+				// 		return "assets/img/svg/[name][extname]";
+				// 	}
+
+				// 	if (/\.js$/.test(assetInfo.name)) {
+				// 		return `assets/js/[name][extname]`;
+				// 	}
+
+				// 	if (/woff|woff2|ttf/.test(assetInfo.name)) {
+				// 		return `assets/fonts/[name][extname]`;
+				// 	}
+				// 	return "assets/[name][extname]";
+				// },
+
+				assetFileNames: (assetInfo) => {
+					if (/\.css$/.test(assetInfo.name)) {
+						return `css/styles.min.css`;
+					} else {
+						return assetInfo.originalFileName ?? assetInfo.name;
+					}
+				},
+
+				entryFileNames: `js/[name].min.js`,
+				chunkFileNames: `js/[name].min.js`,
+			},
+		},
+		outDir: "../dist",
+		emptyOutDir: true,
+	},
+	server: {
+		port: 3000, // Порт локального сервера
+		open: true, // Відкривати браузер при запуску
+		host: true,
+	},
+	optimizeDeps: {
+		entries: "src/**/*{.html,.css,.scss,.js}",
+	},
+
+	plugins: [
+		// tailwindcss(),
+		// noAttr(),
+		// transformImagePaths(process.env.NODE_ENV === "production"),
+		handlebars({
+			partialDirectory: resolve("src", "partials"),
+			reloadOnPartialChange: true,
+
+			context: {
+				
+			},
+
+			// щоб декілька - імпортуємо json файл і через кому додаємо
+			// context: { images, data }
+		}),
+		{
+			// перезавантажуємо сервер після змін у partials
+			handleHotUpdate({ file, server }) {
+				if (file.includes("src/partials") && file.endsWith(".html")) {
+					server.ws.send({
+						type: "full-reload",
+						path: "*",
+					});
+				}
+			},
+		},
+	],
+});
